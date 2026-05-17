59b7ae2c8ea111bca860fe693622f84eff724e8f
Jeremy Klein <jeremy@omnisona.com>
2026-05-17 14:41:46 -0700

    Update shared password to Rush2112

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,2 +1,2 @@
-export const SHARED_PASSWORD = import.meta.env.VITE_SHARED_PASSWORD || 'game-night'
+export const SHARED_PASSWORD = import.meta.env.VITE_SHARED_PASSWORD || 'Rush2112'
 export const AUTH_SESSION_KEY = 'game-night-unlocked'
